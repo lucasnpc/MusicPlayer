ee5132a2dd7ca8c8aefc89e20d5f280c4c3e849b
lucasnpc <31970919+lucasnpc@users.noreply.github.com>
2017-10-26 16:47:14 -0200

    Atualizado

--- app/src/main/java/com/example/cti/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/cti/musicplayer/MainActivity.java
@@ -11,7 +11,10 @@ import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.MediaController;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -19,13 +22,15 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MediaController.MediaPlayerControl {
 
     private ArrayList<Song> songList;
     private ListView songView;
     private MusicService musicSrv;
     private Intent playIntent;
     private boolean musicBound = false;
+    private MusicController controller;
+    private boolean paused=false, playbackPaused=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         });
         SongAdapter songAdt = new SongAdapter(this, songList);
         songView.setAdapter(songAdt);
+        setController();
     }
 
     private ServiceConnection musicConection = new ServiceConnection() {
@@ -84,5 +90,154 @@ public class MainActivity extends AppCompatActivity {
                 songList.add(new Song(thisId,thisTitle,thisArtist));
             }while (musicCursor.moveToNext());
         }
+    }
+
+    public void songPicked(View view){
+        musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
+        musicSrv.playSong();
+        if(playbackPaused){
+            setController();
+            playbackPaused = false;
+        }
+        controller.show(0);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_shuffle:
+                musicSrv.setShuffle();
+                break;
+            case R.id.action_end: stopService(playIntent);
+                musicSrv = null;
+                System.exit(0);
+                break;
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+
+    private void playNext(){
+        musicSrv.playNext();
+        if (playbackPaused){
+            setController();
+            playbackPaused=false;
+        }
+        controller.show(0);
+    }
+
+    private void playPrev(){
+       musicSrv.playPrev();
+        if(playbackPaused){
+            setController();
+            playbackPaused=false;
+        }
+        controller.show(0);
+    }
+
+    private void setController(){
+        controller = new MusicController(this);
+        controller.setPrevNextListeners(new View.OnClickListener() {
+            public void OnClick(View view) {
+                playNext();
+            }
+        }, new View.OnClickListener(){
+            public void onClick(View view){
+                playPrev();
+            }
+        });
+        controller.setMediaPlayer(this);
+        controller.setAnchorView(findViewById(R.id.song_list));
+        controller.setEnabled(true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(playIntent);
+        musicSrv=null;
+        super.onDestroy();
+    }
+
+    @Override
+    public void start() {
+        musicSrv.go();
+    }
+
+    @Override
+    public void pause() {
+        playbackPaused=true;
+        musicSrv.pausePlayer();
+    }
+
+    @Override
+    public int getDuration() {
+        if(musicSrv!=null &amp;&amp; musicBound &amp;&amp; musicSrv.isPng()){
+            return musicSrv.getDur();
+        }else return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        if(musicSrv!=null &amp;&amp; musicBound &amp;&amp; musicSrv.isPng()){
+            return musicSrv.getPosn();
+        }else return 0;
+
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        musicSrv.seek(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if(musicSrv!=null &amp;&amp; musicBound){
+            return musicSrv.isPng();
+        }
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return true;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return true;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+
+    public void onPause(){
+        super.onPause();
+        paused=true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(paused){
+            setController();
+            paused = false;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        controller.hide();
+        super.onStop();
     }
 }
---
+++ app/src/main/java/com/example/cti/musicplayer/MusicController.java
@@ -0,0 +1,16 @@
+package com.example.cti.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Cti on 26/10/2017.
+ */
+
+public class MusicController extends MediaController {
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
