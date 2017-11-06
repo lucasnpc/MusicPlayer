feeadab0d5d1ebb07eae8107094f5fe8711ea6bc
lucasnpc <31970919+lucasnpc@users.noreply.github.com>
2017-11-06 15:39:46 -0200

    Atualizado

--- app/src/main/java/com/example/cti/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/cti/musicplayer/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.cti.musicplayer;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.IBinder;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -21,11 +25,18 @@ import android.view.Window;
 import android.widget.ListView;
 import android.widget.MediaController;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.net.URI;
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity implements MediaController.MediaPlayerControl {
 
@@ -35,17 +46,21 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
     private Intent playIntent;
     private boolean musicBound = false;
     private MusicController controller;
-    private boolean paused=false, playbackPaused=false;
+    private boolean paused = false, playbackPaused = false;
     private Toolbar toolbar;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        songView = (ListView)findViewById(R.id.song_list);
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        songView = (ListView) findViewById(R.id.song_list);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         songList = new ArrayList<Song>();
         getSongList();
         Collections.sort(songList, new Comparator<Song>() {
@@ -57,6 +72,10 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
         SongAdapter songAdt = new SongAdapter(this, songList);
         songView.setAdapter(songAdt);
         setController();
+        setSupportActionBar(toolbar);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -69,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
     private ServiceConnection musicConection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            MusicService.MusicBinder binder = (MusicService.MusicBinder) service;
             musicSrv = binder.getService();
             musicSrv.setList(songList);
             musicBound = true;
@@ -83,38 +102,43 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
 
     @Override
     protected void onStart() {
-        super.onStart();
-        if(playIntent==null){
-            playIntent = new Intent(this,MusicService.class);
-            bindService(playIntent,musicConection, Context.BIND_AUTO_CREATE);
+        super.onStart();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        if (playIntent == null) {
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConection, Context.BIND_AUTO_CREATE);
             startService(playIntent);
         }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
     }
 
-    public void getSongList(){
+    public void getSongList() {
         //Recupera a informação da canção
         ContentResolver musicResolver = getContentResolver();
         Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        Cursor musicCursor = musicResolver.query(musicUri,null,null,null,null);
-        if(musicCursor != null && musicCursor.moveToFirst()){
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        if (musicCursor != null && musicCursor.moveToFirst()) {
             //pega colunas
             int titleColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int idColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media._ID);
             int artistColumn = musicCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
             //adiciona musicas a lista
-            do{
+            do {
                 long thisId = musicCursor.getLong(idColumn);
                 String thisTitle = musicCursor.getString(titleColumn);
                 String thisArtist = musicCursor.getString(artistColumn);
-                songList.add(new Song(thisId,thisTitle,thisArtist));
-            }while (musicCursor.moveToNext());
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            } while (musicCursor.moveToNext());
         }
     }
 
-    public void songPicked(View view){
+    public void songPicked(View view) {
         musicSrv.setSong(Integer.parseInt(view.getTag().toString()));
         musicSrv.playSong();
-        if(playbackPaused){
+        if (playbackPaused) {
             setController();
             playbackPaused = false;
         }
@@ -123,45 +147,46 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_shuffle:
                 musicSrv.setShuffle();
                 break;
-            case R.id.action_end: stopService(playIntent);
+            case R.id.action_end:
+                stopService(playIntent);
                 musicSrv = null;
                 System.exit(0);
                 break;
         }
-        return  super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item);
     }
 
-    private void playNext(){
+    private void playNext() {
         musicSrv.playNext();
-        if (playbackPaused){
+        if (playbackPaused) {
             setController();
-            playbackPaused=false;
+            playbackPaused = false;
         }
         controller.show(0);
     }
 
-    private void playPrev(){
-       musicSrv.playPrev();
-        if(playbackPaused){
+    private void playPrev() {
+        musicSrv.playPrev();
+        if (playbackPaused) {
             setController();
-            playbackPaused=false;
+            playbackPaused = false;
         }
         controller.show(0);
     }
 
-    private void setController(){
+    private void setController() {
         controller = new MusicController(this);
         controller.setPrevNextListeners(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 playNext();
             }
-        }, new View.OnClickListener(){
-            public void onClick(View view){
+        }, new View.OnClickListener() {
+            public void onClick(View view) {
                 playPrev();
             }
         });
@@ -173,7 +198,7 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
     @Override
     protected void onDestroy() {
         stopService(playIntent);
-        musicSrv=null;
+        musicSrv = null;
         super.onDestroy();
     }
 
@@ -184,22 +209,22 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
 
     @Override
     public void pause() {
-        playbackPaused=true;
+        playbackPaused = true;
         musicSrv.pausePlayer();
     }
 
     @Override
     public int getDuration() {
-        if(musicSrv!=null && musicBound && musicSrv.isPng()){
+        if (musicSrv != null && musicBound && musicSrv.isPng()) {
             return musicSrv.getDur();
-        }else return 0;
+        } else return 0;
     }
 
     @Override
     public int getCurrentPosition() {
-        if(musicSrv!=null && musicBound && musicSrv.isPng()){
+        if (musicSrv != null && musicBound && musicSrv.isPng()) {
             return musicSrv.getPosn();
-        }else return 0;
+        } else return 0;
 
     }
 
@@ -210,7 +235,7 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
 
     @Override
     public boolean isPlaying() {
-        if(musicSrv!=null && musicBound){
+        if (musicSrv != null && musicBound) {
             return musicSrv.isPng();
         }
         return false;
@@ -241,15 +266,15 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
         return 0;
     }
 
-    public void onPause(){
+    public void onPause() {
         super.onPause();
-        paused=true;
+        paused = true;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(paused){
+        if (paused) {
             setController();
             paused = false;
         }
@@ -258,6 +283,27 @@ public class MainActivity extends AppCompatActivity implements MediaController.M
     @Override
     protected void onStop() {
         controller.hide();
-        super.onStop();
+        super.onStop();// ATTENTION: This was auto-generated to implement the App Indexing API.
+// See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.disconnect();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
     }
 }
